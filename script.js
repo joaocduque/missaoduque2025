2bb31688775e58a03edf1a858dabcae529b9e1c4
joaocduque <146885443+joaocduque@users.noreply.github.com>
2025-06-26 11:10:31 -0300

    site

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-resultado");
+const caixaReultado = document.querySelector(".texto-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
